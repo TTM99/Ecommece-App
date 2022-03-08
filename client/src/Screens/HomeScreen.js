aea1dd245b2a264fcec63e2c83c411f5e1b95aa1
TTM99 <tarangathapacr7@gmail.com>
2022-03-08 19:25:40 +0545

    Rating and reviews added

--- client/src/Screens/HomeScreen.js
+++ client/src/Screens/HomeScreen.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect, useReducer } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Row, Col } from "react-bootstrap";
 import logger from "use-reducer-logger";
+import Product from "./components/Product";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -54,22 +55,13 @@ const HomeScreen = () => {
         <div>{error}</div>
       ) : productData ? (
         <div className="products">
-          {productData.map((product) => (
-            <div className="product" key={product.slug}>
-              <Link to={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </Link>
-              <div className="product-info">
-                <Link to={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </Link>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
+          <Row>
+            {productData.map((product) => (
+              <Col sm={6} md={4} lg={3} className="mb-3" key={product.slug}>
+                <Product product={product} className="product" />
+              </Col>
+            ))}
+          </Row>
         </div>
       ) : (
         <div>There are no products</div>
